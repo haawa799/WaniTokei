68a79fb246328a3df997f7d7177fda54bc259017
Andrew Kharchyshyn <haawaplus@gmail.com>
2016-12-26 16:29:27 +0200

    Fix launch crash on device

--- WaniTokei/AppDelegate.swift
+++ WaniTokei/AppDelegate.swift
@@ -11,12 +11,5 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
   
-  var window: UIWindow?
-  
-  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-    // Override point for customization after application launch.
-    return true
-  }
-  
 }
 
